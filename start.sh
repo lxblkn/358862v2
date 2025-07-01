3b110a3e560eff39e3b320c9ef2ffd27fb12f927
lxblkn <infoblkn@gmail.com>
2025-07-01 17:25:15 -0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pip install -r requirements.txt
-python3 listener_01.py
+
+HOUR=$(date +%H)
+if [ "$HOUR" -ge 11 ] || [ "$HOUR" -lt 1 ]; then
+    echo "✅ Время в пределах 11:00–01:00. Запускаем парсер..."
+    python listener_01.py
+else
+    echo "⏳ Сейчас не время работы парсера. Завершаем..."
+    exit 0
+fi
